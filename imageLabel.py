e97111bd15926048b6c5daf3aa7f1240c669777f
mr-fool <mr-fool@users.noreply.github.com>
2023-11-08 10:08:18 -0700

    Update imageLabel.py
    add gui interface using Tkinter

--- imageLabel.py
+++ imageLabel.py
@@ -1,15 +1,48 @@
-# Import the Image class from the Pillow (PIL) library
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image
-
-# Load the original image from the specified path
-InputImg = Image.open("/home/hetzer/Pictures/valley.jpg")
-
-# Import the matplotlib library for image visualization
 import matplotlib.pyplot as plt
+from torchvision import transforms
+import torch
+from torchvision import models
 
-# Create a series of image transformations using torchvision.transforms
-# These transformations will resize the image, center crop it, convert it to a tensor,
-# and normalize it with specified mean and standard deviation values.
+# Create a Tkinter application
+app = tk.Tk()
+app.title("Image Classification")
+
+# Function to classify the image
+def classify_image():
+    # Get the image location from the user
+    image_location = entry_image_location.get()
+
+    # Load the original image from the specified path
+    InputImg = Image.open(image_location)
+
+    # Apply the defined transformations to the original image
+    InputImg_t = transform(InputImg)
+
+    # Add a batch dimension to the image tensor
+    InputImg_bt = torch.unsqueeze(InputImg_t, 0)
+
+    # Pass the batched image tensor through the ResNet-152 model to obtain output
+    output = resnet(InputImg_bt)
+
+    # Sort the output tensor to get the top predicted class indices in descending order
+    _, predictedLabels = torch.sort(output, descending=True)
+
+    # Calculate the percentages by applying a sigmoid function to the output tensor
+    percentage = torch.sigmoid(output)[0] * 100
+
+    # Get the user-specified number of labels to display
+    num_labels_to_display = int(entry_num_labels.get())
+
+    # Display the top 'num_labels_to_display' predicted classes and their associated probabilities
+    results_text.delete(1.0, tk.END)  # Clear the previous results
+    top_predictions = [(ImageNetclasses[index], percentage[index].item()) for index in predictedLabels[0][:num_labels_to_display]]
+    for label, prob in top_predictions:
+        results_text.insert(tk.END, f"{label}: {prob:.2f}%\n")
+
+# Create a series of image transformations
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -20,49 +53,36 @@ transform = transforms.Compose([
     )
 ])
 
-# Apply the defined transformations to the original image
-InputImg_t = transform(InputImg)
-
-# Print the shape of the transformed image tensor
-print(InputImg_t.shape)
-
-# Import the torch library for deep learning operations
-import torch
-
-# Add a batch dimension to the image tensor (expected by the model)
-InputImg_bt = torch.unsqueeze(InputImg_t, 0)
-
-# Print the shape of the batched image tensor
-print(InputImg_bt.shape)
-
-# Import the torchvision models module to access pre-trained models
-from torchvision import models
-
-# List the available models in torchvision
-dir(models)
-
 # Load a pre-trained ResNet-152 model and set it to evaluation mode
 resnet = models.resnet152(pretrained=True)
 resnet.eval()
 
-# Pass the batched image tensor through the ResNet-152 model to obtain output
-output = resnet(InputImg_bt)
-
-# Uncomment the following lines to use a pre-trained AlexNet model instead
-# alexnet = models.alexnet(pretrained=True)
-# alexnet.eval()
-# output = alexnet(InputImg_bt)
-
 # Open a file containing ImageNet class labels
-with open('/home/hetzer/Downloads/imagenet1000Classes.txt') as classesfile:
+with open('imagenet1000Classes.txt') as classesfile:
     # Read and store the class labels from the file
     ImageNetclasses = [line.strip() for line in classesfile.readlines()]
 
-# Sort the output tensor to get the top predicted class indices in descending order
-_, predictedLabels = torch.sort(output, descending=True)
+# Create and pack widgets
+label_image_location = tk.Label(app, text="Image Location:")
+label_image_location.pack()
 
-# Calculate the percentages by applying a sigmoid function to the output tensor
-percentage = torch.sigmoid(output)[0] * 100
+entry_image_location = tk.Entry(app)
+entry_image_location.pack()
 
-# Print the top 5 predicted classes and their associated probabilities
-print([(ImageNetclasses[index], percentage[index].item()) for index in predictedLabels[0][:5]])
+label_num_labels = tk.Label(app, text="Number of Labels to Display:")
+label_num_labels.pack()
+
+entry_num_labels = tk.Entry(app)
+entry_num_labels.pack()
+entry_num_labels.insert(0, "5")  # Default value
+
+button_browse = tk.Button(app, text="Browse", command=lambda: entry_image_location.insert(tk.END, filedialog.askopenfilename()))
+button_browse.pack()
+
+button_classify = tk.Button(app, text="Classify", command=classify_image)
+button_classify.pack()
+
+results_text = tk.Text(app, height=10, width=50)
+results_text.pack()
+
+app.mainloop()
